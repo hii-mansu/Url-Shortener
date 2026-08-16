cf81dd4926f03b4dcdefd459b6aaff75a05edb40
Himanshu Singh <himanshusingh5067@gmail.com>
2026-08-17 02:21:54 +0530

    fix: added logic in axios api, aadded axios ninterceptros in axios api to automaticlly retry with creating new token for me

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,12 @@
-import axios from "axios";
-import { getAccessToken } from "./token";
+import axios, {
+  AxiosError,
+  InternalAxiosRequestConfig,
+} from "axios";
+
+import {
+  getAccessToken,
+  setAccessToken,
+} from "./token";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -7,7 +14,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(
-  (config) => {
+  (config: InternalAxiosRequestConfig) => {
     const token = getAccessToken();
 
     if (token) {
@@ -18,6 +25,51 @@ api.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+
+  async (error: AxiosError) => {
+    const originalRequest = error.config as
+      | (InternalAxiosRequestConfig & { _retry?: boolean })
+      | undefined;
+
+    if (
+      error.response?.status !== 401 ||
+      !originalRequest ||
+      originalRequest._retry
+    ) {
+      return Promise.reject(error);
+    }
+
+    originalRequest._retry = true;
+
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+
+      const newAccessToken = response.data.data.accessToken;
+
+      setAccessToken(newAccessToken);
+
+      originalRequest.headers.Authorization =
+        `Bearer ${newAccessToken}`;
+
+      return api(originalRequest);
+    } catch (refreshError) {
+      setAccessToken(null);
+
+      return Promise.reject(refreshError);
+    }
   }
 );
 
